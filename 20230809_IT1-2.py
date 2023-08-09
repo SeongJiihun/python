9c425f8ca3894673002af0d4e872cfb681aeeeca
Ji-hun Seong <q23607019@gmail.com>
2023-08-09 12:41:05 +0900

    .

--- 20230809_IT1-2.py
+++ 20230809_IT1-2.py
@@ -12,10 +12,37 @@
 #         for value in values:
 #             print(value)
 #         print(" ")
-# print_n_times(5, "안녕","하세요","굿나잇")
+# printoutput = output * valu
+# etu
+# return outputt"안녕","하세요","
+# 
+#   굿나잇")
 
-def print_n_times(value,n=2):
-    for i in range(n):
-        print(value)
+# def print_n_times(value,n=2):
+#     for i in range(n):
+#         print(value)
 
-print_n_times("ㅎㅇ")
+# print_n_times("ㅎㅇ")
+
+# while True:
+#     print(".",end="")
+
+# def test(a,b=10,c=100):
+#     print(a+b+c)
+
+# test(10,c=300)
+
+# def return_test():
+#     print("A")
+#     return
+#     print("B")
+
+# return_test()
+
+def mul(*values):
+   output = 1
+   for value in values:
+      output = output * value
+      
+   return output
+print(mul(5, 7, 9, 10)) 
